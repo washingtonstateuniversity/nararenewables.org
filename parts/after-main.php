ca77c85c8327521b780dc640516ac5f4ccc890f8
Charles J Dillon <charles.dillon2@wsu.edu>
2016-09-12 14:26:06 -0700

    Adds images into the footer, adjusts typography, and adds inside page styles

--- parts/after-main.php
+++ parts/after-main.php
@@ -3,11 +3,13 @@
     <p>NARA is led by Washington State University and supported by the Agriculture and Food Research Initiative Competitive Grant no. 2011-68005-30416 from the USDA National Institute of Food and Agriculture.</p>
   </div>
   <div class="logos">
-    <img class="footer-logo" src="/assets/usda-logo.png" width="198" width="198" alt="UCDA"> <img class="footer-logo" src="/assets/wsu-cougar-head.png" width="198" width="198" alt="Washington State University">
+    <img class="footer-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/usda-logo.png" width="198" width="198" alt="UCDA"> <img class="footer-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wsu-cougar-head.png" width="198" width="198" alt="Washington State University">
   </div>
   <div class="social-media">
     <header>Follow us</header>
-    <img class="social-logo" src="/assets/twitter.png" width="32" height="32" alt="NARA Twitter" > <img class="social-logo" src="/assets/facebook.png" width="32" height="32" alt="NARA Facebook" >
-    <img class="social-logo" src="/assets/youtube.png" width="32" height="32" alt="NARA YouTube" .
+    <a href="https://twitter.com/NARA_Renewables"><img class="social-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/twitter.png" width="32" height="32" alt="NARA Twitter" ></a>
+    <a href="https://www.facebook.com/pages/Northwest-Advanced-Renewables-Alliance/232111166837523"><img class="social-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/facebook.png" width="32" height="32" alt="NARA Facebook" ></a>
+    
+    <a href="https://www.youtube.com/channel/UC4Uqugx94eUudAajQ3e_MnQ"><img class="social-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/youtube.png" width="32" height="32" alt="NARA YouTube"></a>
   </div>
 </footer>
