d6fc36f945d92357928ea94c3ed676d117ac7314
Charles J Dillon <charles.dillon2@wsu.edu>
2016-09-12 13:40:37 -0700

    Initial commit

---
+++ parts/after-main.php
@@ -0,0 +1,13 @@
+<footer>
+  <div class="copy">
+    <p>NARA is led by Washington State University and supported by the Agriculture and Food Research Initiative Competitive Grant no. 2011-68005-30416 from the USDA National Institute of Food and Agriculture.</p>
+  </div>
+  <div class="logos">
+    <img class="footer-logo" src="/assets/usda-logo.png" width="198" width="198" alt="UCDA"> <img class="footer-logo" src="/assets/wsu-cougar-head.png" width="198" width="198" alt="Washington State University">
+  </div>
+  <div class="social-media">
+    <header>Follow us</header>
+    <img class="social-logo" src="/assets/twitter.png" width="32" height="32" alt="NARA Twitter" > <img class="social-logo" src="/assets/facebook.png" width="32" height="32" alt="NARA Facebook" >
+    <img class="social-logo" src="/assets/youtube.png" width="32" height="32" alt="NARA YouTube" .
+  </div>
+</footer>
